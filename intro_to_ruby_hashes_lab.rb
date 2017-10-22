8c60fb895910ec0674fec5abed745d259429056e
Ryan Manchester <ryanm17@gmail.com>
2017-10-22 19:40:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,12 +8,21 @@ def actor
 end
 
 def monopoly
-	monopoly = {:railroads => {}}
+	monopoly = {
+		:railroads => {}
+	}
 
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {pieces: 4, names:{}, rent_in_dollars:{}}}
+	monopoly = {
+		:railroads => 
+		{
+			pieces: 4, 
+			names:{}, r
+			ent_in_dollars:{}
+			}
+		}
 
 end
 
